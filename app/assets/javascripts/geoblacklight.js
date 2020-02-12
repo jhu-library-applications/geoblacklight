319c52e4b8a619e68917ddb2f2ca8d33a5216c52
Eric Larson <ewlarson@gmail.com>
2020-02-12 09:48:58 -0600

    Basemap: use the ESRI basemap

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -8,3 +8,12 @@
 //= require leaflet-iiif
 //= require esri-leaflet
 //= require readmore
+
+// additional leaflet base layers
+GeoBlacklight.Basemaps.esri =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: false,
+  maxZoom: 18,
+  worldCopyJump: true,
+  detectRetina: true,
+  noWrap: false
+});
