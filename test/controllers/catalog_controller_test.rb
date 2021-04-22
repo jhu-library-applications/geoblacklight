1e16670a6c44df3a1f7e0dde8e607bcaf8fbf9a6
Eric Larson <ewlarson@gmail.com>
2021-04-22 14:36:31 -0500

    Gems: Rails 6.1
    Support for Rails 6.1 here.
    
    Author:    Eric Larson <ewlarson@gmail.com>
    Date:      Thu Apr 22 14:36:31 2021 -0500

--- test/controllers/catalog_controller_test.rb
+++ test/controllers/catalog_controller_test.rb
@@ -4,7 +4,7 @@ class CatalogControllerTest < ActionDispatch::IntegrationTest
   test "should return centroids in json results" do
     get '/catalog', params: { format: 'json' }
     assert_response :success
-    assert_equal 'application/json', response.content_type
+    assert_equal 'application/json; charset=utf-8', response.content_type
 
     json = JSON.parse(response.body)
     refute_empty json["centroids"]
