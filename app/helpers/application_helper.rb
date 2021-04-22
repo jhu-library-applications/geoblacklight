dc607732a4f8ce75e49bee15301bccac0962cb53
Jamie Little <jamie@jamielittle.org>
2021-04-22 10:49:34 -0500

    Update to Rails 6 and Ruby 2.7
    This also includes a fix for a potential issue
    with the CSRF token. The method for generating
    that changed in Rails 6 and may cause an error
    if a user has an existing session. The fix catches
    the potential error and starts a new session.
    
    Closes #84

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,13 @@ module ApplicationHelper
 
   # Always return an array of geom type values
   def geom_types(types)
-      types = [*types]
+    types = [*types]
+  end
+
+  def csrf_meta_tags
+    super
+  rescue ArgumentError
+    request.reset_session
+    super
   end
 end
