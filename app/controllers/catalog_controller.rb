c40c87e5a83d76a185006b60da9f6bb338d14a2e
Eric Larson <ewlarson@gmail.com>
2021-04-23 15:29:41 -0500

    Bug fix: relation browsing requires a defined facet field (#97)
    Fixes #95

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -127,6 +127,8 @@ class CatalogController < ApplicationController
 
     config.add_facet_field Settings.FIELDS.TYPE, label: 'Type', limit: 8
 
+    config.add_facet_field Settings.FIELDS.SOURCE, :label => 'Source', :limit => 8, collapse: false
+
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
     # handler defaults, or have no facets.
